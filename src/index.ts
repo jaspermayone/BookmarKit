108e25c3843fbd2c6b931dba093675928bfb5898
Jasper Mayone <jasper.mayone@singlefeather.com>
2023-10-14 10:03:48 -0400

    fix: permission problems

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,39 @@
-document.getElementById('saveTabs')!.addEventListener('click', function() {
-  chrome.tabs.query({}, function(tabs) {
-    const urls = tabs.map(tab => tab.url);
-    chrome.storage.sync.set({ savedTabs: urls }, function() {
-      alert('Tabs saved successfully! \u2714');
+function saveTabs() {
+  chrome.identity.getProfileUserInfo(function(userInfo) {
+    const userId = userInfo.id;
+
+    chrome.tabs.query({}, function(tabs) {
+      const urls = tabs.map(tab => tab.url);
+
+      chrome.storage.sync.set({ userId, savedTabs: urls }, function() {
+        alert('Tabs saved successfully! \u2714');
+      });
     });
   });
-});
+}
+
+document.getElementById('saveTabs')!.addEventListener('click', saveTabs);
 
 document.getElementById('retrieveTabs')!.addEventListener('click', function() {
-  chrome.storage.sync.get(['savedTabs'], function(data) {
-    const savedTabs = data.savedTabs;
-    if (savedTabs) {
-      savedTabs.forEach((url: string) => {
-        chrome.tabs.create({ url });
-      });
-    } else {
-      alert('No saved tabs found! \u274C');
-    }
+  chrome.identity.getProfileUserInfo(function(userInfo) {
+    const userId = userInfo.id;
+
+    chrome.storage.sync.get(['userId', 'savedTabs'], function(data) {
+      // Check if the user is authorized
+      if (data.userId === userId) {
+        // Retrieve the saved tabs
+        const savedTabs = data.savedTabs;
+        // Open each saved tab
+        if (savedTabs && savedTabs.length > 0) {
+          savedTabs.forEach((url: string) => {
+            chrome.tabs.create({ url });
+          });
+        } else {
+          alert('No saved tabs found! \u274C');
+        }
+      } else {
+        alert('You are not authorized to retrieve these tabs! \u274C');
+      }
+    });
   });
 });
